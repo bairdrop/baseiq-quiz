7b2ff649877d3ddebafc8a7beb453ef3a5bfbc48
bairdrop <116457570+bairdrop@users.noreply.github.com>
2025-11-02 17:18:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Disable external analytics
+    'process.env.VITE_DISABLE_ANALYTICS': JSON.stringify('true')
+  },
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob:; connect-src 'self';"
+    }
+  }
 })
