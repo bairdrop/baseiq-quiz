e5458f58670e8da6bb8518ff5095197809f845ee
bairdrop <116457570+bairdrop@users.noreply.github.com>
2025-11-02 17:35:23 +0100

    Create minikit.config.ts

---
+++ minikit.config.ts
@@ -0,0 +1,16 @@
+export const minikitConfig = {
+  accountAssociation: {
+    header: "eyJmaWQiOjUyNjk5NiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ4YURERTk1ZkY1OGNjQzRBRTkxYjM3YzY4NkVmQTA3OTFhMDUxMDcifQ",
+    payload: "eyJkb21haW4iOiJiYXNlaXEtc2V2ZW4udmVyY2VsLmFwcCJ9",
+    signature: "0Y7W6Fv05SKmv/oazJIPXUwZlrjFfcHcWNQKyVh6ogRiarUxKBVfVZ2CHhxNVbL09fbV5ixbIbiIDmq4cNor6xw="
+  },
+  miniapp: {
+    name: "BaseIQ",
+    version: "1.0.0",
+    iconUrl: "https://baseiq-seven.vercel.app/icon.png",
+    homeUrl: "https://baseiq-seven.vercel.app",
+    imageUrl: "https://baseiq-seven.vercel.app/hero.png",
+    splashImageUrl: "https://baseiq-seven.vercel.app/icon.png",
+    splashBackgroundColor: "#000000"
+  }
+};
